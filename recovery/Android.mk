d534bf061c309d0f10dfa6983711ca4f585720f1
HomuHomu <ecoco666@gmail.com>
2016-01-30 23:05:25 +0900

    twrp: fix SRC FILES PATH
    Change-Id: I7460c86a53c25ec848f8fd2c70476219d3c9f7da

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,7 +2,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := twrp.fstab
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := recovery/twrp.fstab
+LOCAL_SRC_FILES    := ../recovery/twrp.fstab
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/etc
 include $(BUILD_PREBUILT)
 
@@ -10,6 +10,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := postrecoveryboot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := recovery/postrecoveryboot.sh
+LOCAL_SRC_FILES    := ../recovery/postrecoveryboot.sh
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
