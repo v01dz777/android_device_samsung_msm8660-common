a58c8951ff3b2f3c51c0fc20cc13ddf5200cb084
HomuHomu <ecoco666@gmail.com>
2016-01-31 17:05:31 +0900

    msm8660: remove DeviceSettings app
    Change-Id: Icfe2bcbacacf6ee2f57a47594901b279dc5f3834

--- msm8660.mk
+++ msm8660.mk
@@ -181,8 +181,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
 # MSM8660Settings
-PRODUCT_PACKAGES += \
-    MSM8660Settings
+# PRODUCT_PACKAGES += \
+#    MSM8660Settings
 
 # NFC
 PRODUCT_PACKAGES += \
